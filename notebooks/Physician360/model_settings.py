a38dffacdbb321bcb9fa90c188c3e6b8a253be67
kdegrave <54145798+kdegrave@users.noreply.github.com>
2022-02-10 14:44:07 -0500

    Update model_settings.py

--- notebooks/Physician360/model_settings.py
+++ notebooks/Physician360/model_settings.py
@@ -1,5 +1,5 @@
-topic_classifier_path = '/Users/kyledegrave/PycharmProjects/Physician360/TopicModel/'
-sentiment_classifier_path = '/Users/kyledegrave/PycharmProjects/Physician360/SentimentModel/'
+topic_classifier_path = 'Physician360/TopicModel/'
+sentiment_classifier_path = 'Physician360/SentimentModel/'
 
-log_file_path = '/Users/kyledegrave/PycharmProjects/Physician360/'
-out_file_path = '/Users/kyledegrave/Desktop/'
+log_file_path = 'Physician360/'
+out_file_path = 'Physician360/'
