a3c6e842c5d4f32f7ed6757d2c5cf0e14c65d9cd
kdegrave <54145798+kdegrave@users.noreply.github.com>
2022-02-10 14:49:59 -0500

    Delete settings.py

--- notebooks/Physician360/settings.py
+++
@@ -1,5 +0,0 @@
-topic_classifier_path = '/Users/kyledegrave/PycharmProjects/Physician360/TopicModel/'
-sentiment_classifier_path = '/Users/kyledegrave/PycharmProjects/Physician360/SentimentModel/'
-
-log_file_path = '/Users/kyledegrave/PycharmProjects/Physician360/'
-out_file_path = '/Users/kyledegrave/Desktop/'
